b202c11a0d884987b31a7d1b6e6e0e84947320f5
Vitoria Pereira Klapowsko <99929591+VitoriaPereiraKlapowsko@users.noreply.github.com>
2023-03-16 00:02:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const swtcher = document.querySelector('.btn');
+
+swtcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == 'light-theme') {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    } console.log('current class name: ' + className); 
+});
