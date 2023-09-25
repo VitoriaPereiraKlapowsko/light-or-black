2ef630f6462a4225576b91ff9608f13341103ae7
Vitoria Pereira Klapowsko <vitoriapereiraklapowsko@gmail.com>
2023-09-25 20:43:38 -0300

    Commit Final

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-'use strict'
+'use strict';
 
-const swtcher = document.querySelector('.btn');
+let visitCount = parseInt(localStorage.getItem('visitCount')) || 0;
+visitCount++;
+localStorage.setItem('visitCount', visitCount.toString());
 
-swtcher.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme')
+const msgElement = document.getElementById('msg');
+msgElement.textContent = `Total de visitas: ${visitCount}`;
 
-    var className = document.body.className;
-    if (className == 'light-theme') {
-        this.textContent = "Dark";
+const switcher = document.querySelector('.btn');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    body.classList.add(currentTheme);
+} else {
+    body.classList.add('light-theme'); 
+}
+
+switcher.addEventListener('click', function () {
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark-theme');
+        switcher.textContent = 'Light'; 
     } else {
-        this.textContent = "Light";
-    } console.log('current class name: ' + className);
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        localStorage.setItem('theme', 'light-theme');
+        switcher.textContent = 'Dark'; 
+    }
 });
